812d63013acdd069bcf006730383c26abb2b8eab
motty93 <rdwbocungelt5@gmail.com>
2020-06-20 18:21:04 +0900

    update: read lib module

--- config/application.rb
+++ config/application.rb
@@ -26,6 +26,7 @@ module Myapp
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.paths.add 'lib', eager_load: true
 
     # Don't generate system test files.
     config.generators.system_tests = nil
