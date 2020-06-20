45256fd6b339ae8cd4050abd5a4afd5803c4ef76
motty93 <rdwbocungelt5@gmail.com>
2020-06-20 18:05:57 +0900

    add: bigquery module

---
+++ lib/big_query/auth.rb
@@ -0,0 +1,36 @@
+require "google/cloud/bigquery"
+
+module BigQuery
+  class Auth
+    attr_reader :bigquery, :dataset, :table, :table_name
+
+    DEFAULT_DATASET = 'test'
+
+    def initialize(args={})
+      @bigquery = set_creadential
+      @dataset = set_dataset(args[:dataset] || DEFAULT_DATASET)
+      post_initialize(args)
+    end
+
+    def post_initialize(args)
+      nil
+    end
+
+    private
+
+      def set_creadential
+        keyfile = ENV['BQ_CREDENTIAL_PATH']
+        creds = Google::Cloud::Bigquery::Credentials.new(keyfile)
+        Google::Cloud::Bigquery.new(
+          project_id: ENV['PROJECT_ID'],
+          credentials: creds
+        )
+      end
+
+      def set_dataset(name)
+        return bigquery.dataset(name) if bigquery.datasets.map(&:dataset_id).include?(name)
+
+        bigquery.create_dataset(name, name: name)
+      end
+  end
+end
---
+++ lib/big_query/table.rb
@@ -0,0 +1,25 @@
+module BigQuery
+  class Table < Auth
+
+    def post_initialize(args)
+      @table = exist_table || create_table
+      @table_name = table_name
+    end
+
+    def add(**field_params)
+      table.insert([field_params.merge(created_at: DateTime.now)])
+    end
+
+    def exist_table
+      dataset.table(table_name) if dataset.tables.map(&:table_id).include?(table_name)
+    end
+
+    def create_table
+      raise NotImplementedError, "This #{self.class} cannot respond to: "
+    end
+
+    def table_name
+      self.class.name.demodulize.underscore
+    end
+  end
+end
