04dbfebde52ee493f8dd927c4fbed1e114d85f59
motty93 <rdwbocungelt5@gmail.com>
2020-07-01 23:01:09 +0900

    update: 同期処理 controller

--- app/controllers/api/big_queries_controller.rb
+++ app/controllers/api/big_queries_controller.rb
@@ -1,4 +1,8 @@
 class Api::BigQueriesController < ApplicationController
+  TABLES = [Category, User].freeze
+
   def index
+    client = BigQuery::SyncTable.new(tables: TABLES)
+    client.synchronize
   end
 end
