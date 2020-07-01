f7601b2cb2fa83b2f2329b3567b988c9e9bd19cf
motty93 <rdwbocungelt5@gmail.com>
2020-07-01 23:00:56 +0900

    add: 同期フラグ追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_28_053207) do
+ActiveRecord::Schema.define(version: 2020_06_28_075221) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2020_06_28_053207) do
     t.string "border_color"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "synchronized", default: false, null: false
   end
 
   create_table "users", force: :cascade do |t|
@@ -32,6 +33,7 @@ ActiveRecord::Schema.define(version: 2020_06_28_053207) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "synchronized", default: false, null: false
   end
 
 end
