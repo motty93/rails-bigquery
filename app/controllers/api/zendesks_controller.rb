74bbcc507398e04935e92ff23b5d592bc1976172
motty93 <rdwbocungelt5@gmail.com>
2020-07-22 11:45:09 +0900

    update: zendesk controller & view

--- app/controllers/api/zendesks_controller.rb
+++ app/controllers/api/zendesks_controller.rb
@@ -1,8 +1,7 @@
-class Api::ZendeskController < ApplicationController
+class Api::ZendesksController < ApplicationController
   def show
     url = "https://#{ENV['SUB_DOMAIN']}.zendesk.com/api/v2/help_center/ja/articles/#{params[:id]}.json"
     req = Typhoeus::Request.get(url)
-    response_body = JSON.parse(req.response_body)
-    binding.pry
+    @article = JSON.parse(req.response_body)['article']
   end
 end
