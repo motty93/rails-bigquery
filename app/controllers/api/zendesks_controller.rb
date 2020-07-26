141e9028ea6f18d56ff178a15a3782edb847464a
motty93 <rdwbocungelt5@gmail.com>
2020-07-26 17:36:26 +0900

    update: params

--- app/controllers/api/zendesks_controller.rb
+++ app/controllers/api/zendesks_controller.rb
@@ -2,18 +2,19 @@ class Api::ZendesksController < ApplicationController
   before_action :set_zendesk_help
 
   def show
-    @info = @help_center.article_info.transform_keys { |key| key.camelize(:lower) }
+    @info = @help_center.article_info.transform_keys { |key| key.to_s.camelize(:lower) }
   end
 
   private
 
     def set_zendesk_help
-      @help_center = Zendesk::HelpCenter.new(**article_id_params)
+      @help_center = Zendesk::HelpCenter.new(**article_params)
     end
 
-    def article_id_params
+    def article_params
       {
         article_id: params[:id],
+        locale: params[:locale]
       }
     end
 end
